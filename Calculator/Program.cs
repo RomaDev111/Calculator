9de4441b444a2fe8f55a206286f1a638536292ff
RomaDev111 <K_ramazon12@mail.ru>
2024-02-25 01:29:53 +0500

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Calculator;
+using static Calculator.Operations;
+
+namespace Lesson12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Calculator");
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose operation:");
+            Console.WriteLine("1. Addition (+)");
+            Console.WriteLine("2. Subtraction (-)");
+            Console.WriteLine("3. Multiplication (*)");
+            Console.WriteLine("4. Division (/)");
+
+            Console.Write("Enter the operation number: ");
+            string choice = (Console.ReadLine());
+
+            double result = 0;
+
+
+            switch (choice)
+            {
+                case "+":
+                    result =  Operations.Add(num1, num2);
+                    break;
+                case "-":
+                    result = Operations.Substract(num1, num2);
+                    break;
+                case "*":
+                    result = Operations.Multiply(num1, num2);
+                    break;
+                case "/":
+                    result = Operations.Divide(num1, num2);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    return;
+            }
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
---
+++ Calculator/operations.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Operations
+    {
+        public static double Add(double num1, double num2)
+        { 
+            return num1 + num2;
+        }
+
+        public static double Substract(double num1, double num2) 
+        {
+            return num1 - num2;       
+        }
+
+        public static double Multiply (double num1, double num2)
+        {
+            return num1 * num2; 
+        }
+
+        public static double Divide (double num1, double num2)
+        {
+            if (num2 == 0) 
+            {
+                Console.WriteLine( "Error:Division by zero.");
+                return double.NaN;
+            }
+            return num1 / num2; 
+        }
+
+
+    }
+}
